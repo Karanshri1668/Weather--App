d27915a204f75d7e3dffea8b428e778d098c0d0b
Karan <134969711+Karanshri1668@users.noreply.github.com>
2024-04-17 17:15:28 +0530

    Update WA-script.js

--- WA-script.js
+++ WA-script.js
@@ -7,6 +7,10 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+     if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }else{
     var data = await response.json();
 
     console.log(data);
@@ -36,6 +40,8 @@ async function checkWeather(city){
     }
 
     document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+     }
 }
 
 searchBtn.addEventListener("click", ()=>{
